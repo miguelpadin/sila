b413b5d41de850b31e8f2fe0488f2153f561d74b
Miguel Padin <miguel.padin.dominguez@gmail.com>
2026-06-28 22:22:35 +0100

    perf: enable HTML compression in Astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://www.silapadin.com',
   output: 'static',
-  compressHTML: false,
+  compressHTML: true,
   build: {
     format: 'file',
     inlineStylesheets: 'always',
